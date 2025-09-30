6ee1dded8cf4063c403d5860a0f6f612da8613e6
MatthewWang <mattheww227@nycstudents.net>
2025-09-30 12:22:03 -0400

    first commit

---
+++ src/AppointmentBook.java
@@ -0,0 +1,30 @@
+public class AppointmentBook
+{
+    private boolean[][] schedule;
+
+    public AppointmentBook(boolean[][] schedule)
+    {
+        this.schedule = schedule;
+    }
+
+    private boolean isMinuteFree(int period, int minute)
+    {
+        return schedule[period - 1][minute];
+    }
+
+    public int findFreeBlock(int period, int duration)
+    {
+        return 0;
+    }
+
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
+    {
+        return false;
+    }
+
+    public void printPeriod(int period)
+    {
+        for(int i = 0; i < schedule[period - 1].length; i++)
+            System.out.println(i + " " + schedule[period - 1][i]);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+    boolean[][] schedule = new boolean[8][60];
+    AppointmentBook a = new AppointmentBook(schedule);
+
+    for(int i = 10; i < 15; i++) schedule[1][i] = true;
+}
