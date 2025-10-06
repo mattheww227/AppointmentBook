95fd1d06ade262e61a21bfbd42821cdcbb1a3225
mattheww227 <mattheww227@nycstudents.net>
2025-10-05 23:04:14 -0400

    finished code

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -14,11 +14,34 @@ public class AppointmentBook
 
     public int findFreeBlock(int period, int duration)
     {
-        return 0;
+        int block = 0;
+
+        for(int i = 0; i < 60; i++)
+        {
+            if(isMinuteFree(period, i))
+            {
+                block++;
+                if(block == duration)
+                {
+                    return i - duration + 1;
+                }
+            }
+            else block = 0;
+        }
+        return -1;
     }
 
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
     {
+        for(int i = startPeriod; i <= endPeriod; i++)
+        {
+            int freeBlock = findFreeBlock(i, duration);
+            if(freeBlock > -1)
+            {
+                reserveBlock(freeBlock, duration, i);
+                return true;
+            }
+        }
         return false;
     }
 
@@ -26,5 +49,10 @@ public class AppointmentBook
     {
         for(int i = 0; i < schedule[period - 1].length; i++)
             System.out.println(i + " " + schedule[period - 1][i]);
+    }
+
+    public void reserveBlock(int startMinute, int duration, int period)
+    {
+        for(int i = startMinute; i < startMinute + duration; i++) schedule[period - 1][i] = false;
     }
 }
